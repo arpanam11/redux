78c296cf83e8161053a2d61538aed8131f17f527
arpanam11 <arpanameshram11@gmail.com>
2024-07-09 17:36:12 +0530

    add both store in one code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './reduxToolkit/store';
+//import store from './reduxContainer/Store'
 import AppComponent from '../src/reduxToolkit/AppComponent';
 import ProductContainer from './reduxContainer/ProductContainer';
 
